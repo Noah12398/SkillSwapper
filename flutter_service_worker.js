86b02021bfe3a5cc97b4d76a07056a41902ee3f0
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2025-06-23 12:56:44 +0530

    Deploy latest build with env file

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "967d70887ccc65e6ff9bffc32d2561d3
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "17015d43b1ded73cf3c3cc2a798a6b12",
+"flutter_bootstrap.js": "f94d3abeb52b99654e50849f84111542",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
